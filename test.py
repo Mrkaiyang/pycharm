7b5b98e7367d271d2ee6eb4465e76cec6aaa7323
Hll <qingdahll@163.com>
2019-07-28 17:02:15 +0800

    add new content

---
+++ test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+def hello_test():
+    #TODO print
+    print("Hello world")
+
+
+def test_debug():
+    a = 1
+    b = -2
+    c = a+b
+    d = abs(c)
+    print(d)
+
+
+if __name__ == '__main__':
+    hello_test()
+    test_debug()
